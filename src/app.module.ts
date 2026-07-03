d38f7bd38648a4b815d3b6df624b91d2967baeea
Pennyyyyyyyyy <112306088@g.nccu.edu.tw>
2026-07-03 16:59:40 +0800

    chore: switch database from SQLite to MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -9,6 +11,9 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), 
+    }),
     AuthModule,
     UsersModule,
     PrismaModule,
